6fd2abea504db838db9c6d916e5d59257d5f8fe3
EastComplete <indigoguru@gmail.com>
2015-01-11 02:44:15 +0900

    my first asyn io is committed

--- program4.js
+++ program4.js
@@ -1,41 +1,52 @@
 
-
-/// test git test
-
 var fs = require('fs');
 var  buf = new Buffer(256);
-buf =  fs.readFileSync(process.argv[2]);
+//buf =  fs.readFileSync(process.argv[2]);
+var str = undefined
+var lines = undefined
+var newLineCount = undefined
 
-
-function ReadFile(var filename)
+function ReadFile( filename)
 {
-	
+	fs.readFile(filename, function doneReading(err, fileContents)
+	{
+		str = 	fileContents.toString();
+		lines = str.split('\n');
+
+		newLineCount = Number(lines.length) -1;
+
+
+		console.log (newLineCount);
+
+
+	})
 }
 
+ReadFile(process.argv[2]);
 
-
-
-
-var str = buf.toString();
-//console.log(str);
-
-var result = str.split('\n');
-
-/*
-for ( var i = 0; i < result.length ; i++)
-{
-	console.log(result[i]);
-
-}
-*/ 
 
 /* 
 사유  newline이 1개 있으면, split 된 문자열이 2개임    
 예를 들어   a \n  b 가 있다면   split 된 것이 2개 
 그래서 split된 문자 배열에서 -1을 한것이 newline임 
 
-*/
-var newlinecount = Number(result.length) -1;
+
+var newlinecount = Number(lines.length) -1;
 
 
-console.log (newlinecount);
+console.log (newline.count);
+
+ */
+
+/*
+ var str = buf.toString();
+ //console.log(str);
+
+ var result = str.split('\n');
+
+ for ( var i = 0; i < result.length ; i++)
+ {
+ console.log(result[i]);
+
+ }
+ */
