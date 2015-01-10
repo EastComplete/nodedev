4f32ff3e030ed9b529343fc70174a92423f9647b
EastComplete <indigoguru@gmail.com>
2015-01-11 03:18:47 +0900

    program4.js code copy&paste program5.js

---
+++ program5.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var  buf = new Buffer(256);
+//buf =  fs.readFileSync(process.argv[2]);
+var str = undefined
+var lines = undefined
+var newLineCount = undefined
+
+function ReadFile( filename)
+{
+	fs.readFile(filename, function doneReading(err, fileContents)
+	{
+		str = 	fileContents.toString();
+		lines = str.split('\n');
+
+		newLineCount = Number(lines.length) -1;
+
+
+		console.log (newLineCount);
+
+
+	})
+}
+
+ReadFile(process.argv[2]);
+
+
+
